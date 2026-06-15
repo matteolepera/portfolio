8f45ae57de2edbbf7dff45a6b4e53477ef8a2e35
matteolpera <matteo.lepera02@gmail.com>
2026-06-15 10:29:53 +0200

    feat: aggiunta la bozza all'hero.
    wp sui testi della hero.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Maintenance from "./pages/Maintenance"
+import DefaultLayouts from "./layouts/DefaultLayouts"
+import Home from "./pages/Home"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route element={<Maintenance />} path="/" />
+          <Route element={<DefaultLayouts />}>
+            <Route element={<Maintenance />} path="/" />
+            <Route element={<Home />} path="/home" />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+    return (
+        <div className={styles.hero}>
+            <div className={styles.heroContent}>
+                <span>
+                    AVAILABLE
+                </span>
+
+                <h1>
+                    OPEN TO WORK
+                </h1>
+
+                <p>
+                    Matteo Le Pera
+                </p>
+            </div>
+        </div>
+    )
+}
