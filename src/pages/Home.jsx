27457090567e40b22235e1befc2ba9358f2286f4
matteolpera <matteo.lepera02@gmail.com>
2026-06-15 11:31:55 +0200

    feat: aggiunto scheletro sezione "about me"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,48 @@ import styles from "../styles/Home.module.css"
 
 export default function Home() {
     return (
-        <div className={styles.hero}>
-            <div className={styles.heroContent}>
-                <span>
-                    LIGHTS OUT
-                </span>
+        <>
+            {/* Hero */}
+            <div className={styles.hero}>
+                <div className={styles.heroContent}>
+                    <span>
+                        LIGHTS OUT
+                    </span>
 
-                <h1>
-                    WEB DEVELOPER
-                </h1>
+                    <h1>
+                        WEB DEVELOPER
+                    </h1>
 
-                <p>
-                    Matteo Le Pera
-                </p>
+                    <p>
+                        Matteo Le Pera
+                    </p>
+                </div>
             </div>
-        </div>
+            {/* About Me */}
+            <section>
+                <h2>About Me</h2>
+                <div>
+                    <div>
+                        <h3>Formazione</h3>
+                        <ul>
+                            <li>Boolean</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Progetti</h3>
+                        <ul>
+                            <li>Focus One</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Esperienze</h3>
+                        <ul>
+                            <li>Decathlon</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </>
+
     )
 }
