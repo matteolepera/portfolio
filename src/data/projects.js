d5782cba57d42d2668ce23da52af95939fed4443
matteolpera <matteo.lepera02@gmail.com>
2026-06-17 10:44:21 +0200

    feat: aggiunta la cartella data.

---
+++ src/data/projects.js
@@ -0,0 +1,4 @@
+export const projects = [
+    { id: 1, name: "FocusOne", type: "full stack" },
+    { id: 2, name: "Aeterna Skin", type: "full stack" },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,10 @@
-import styles from "../styles/Home.module.css"
+{/* --Style-- */ }
+import styles from "../styles/Home.module.css";
 
-const stackTechnologies = [
-    { position: 1, logo: "php", code: "PHP", interval: "Leader", tyres: "soft", logoColor: "#777bb3" },
-    { position: 2, logo: "laravel", code: "LAR", interval: "+0.284", tyres: "medium", logoColor: "#ff2d20" },
-    { position: 3, logo: "javascript", code: "JAV", interval: "+0.612", tyres: "soft", logoColor: "#f7df1e", logoForeground: "#111111" },
-    { position: 4, logo: "react", code: "REA", interval: "+0.944", tyres: "medium", logoColor: "#61dafb", logoForeground: "#111111" },
-    { position: 5, logo: "bootstrap", code: "BOO", interval: "+1.288", tyres: "hard", logoColor: "#7952b3" },
-    { position: 6, logo: "nodejs", code: "NOD", interval: "+1.633", tyres: "hard", logoColor: "#3c873a" },
-    { position: 7, logo: "expressjs", code: "EXP", interval: "+1.981", tyres: "medium", logoColor: "#1f1f1f" },
-    { position: 8, logo: "html", code: "HTM", interval: "+2.315", tyres: "soft", logoColor: "#e34f26" },
-    { position: 9, logo: "css", code: "CSS", interval: "+2.704", tyres: "medium", logoColor: "#1572b6" },
-    { position: 10, logo: "mysql", code: "SQL", interval: "+3.011", tyres: "hard", logoColor: "#4479a1" },
-]
+{/* --Array-- */ }
+import { stackTechnologies } from "../data/stackTechnologies.js";
+import { projects } from "../data/projects.js";
+
 
 export default function Home() {
     return (
